1e02bc7c79d134f5642b38b5d7a8c553c4cc49c7
Dmitry Voronkevich <kaducei2020@gmail.com>
2022-11-13 23:13:01 +0300

    newUltraFix

---
+++ src/components/App/kekus.js
@@ -0,0 +1,133 @@
+function handlePhoto(errorerrorerrorerrorerrorerrorerrorerror) {
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  if (errorerrorerrorerrorerrorerrorerrorerror) {
+    return errorerrorerrorerrorerrorerrorerrorerror;
+  }
+  return errorerrorerrorerrorerrorerrorerrorerror;
+}
+export default handlePhoto;
